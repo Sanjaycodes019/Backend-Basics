03442833a731a9b6ff2782dfa476c7acdb2cf71f
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-07-01 13:06:56 +0000

    commit

--- modules/fs.js
+++ modules/fs.js
@@ -1,3 +1,27 @@
+// reading file sync
 const fs = require('fs');
+console.log('first');
 const file = fs.readFileSync('file.txt', 'utf-8');
 console.log(file);
+
+console.log('second')
+
+// reading file async
+fs.readFile('file.txt','utf-8', (err, data)=>{
+    if(err){
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
+console.log('third');
+
+// writing file (creating)
+fs.writeFileSync('./greet.txt','Hello world');
+
+
+fs.writeFile('./greet.txt', 'Hello Sanjay', (err)=>{
+    if(!err){
+        console.log('file logged!')
+    }
+})
