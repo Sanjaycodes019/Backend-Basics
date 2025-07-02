b2b7b3fa152eb012b0e567139c022a2be866e3b7
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-07-02 09:37:04 +0000

    commit

--- modules/fs/assignment2.js
+++ modules/fs/assignment2.js
@@ -1,24 +1,37 @@
-// async code
-const fs = require('fs');
-fs.readFile('greet.txt', 'utf-8', (err, data1) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  fs.readFile('file.txt', 'utf-8', (err, data2) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
+// // async code
+// const fs = require('fs');
+// fs.readFile('greet.txt', 'utf-8', (err, data1) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   fs.readFile('file.txt', 'utf-8', (err, data2) => {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
 
+//     const finalContent = data1 + '\n' + data2;
+
+//     fs.writeFile('thirdfile.txt', finalContent, (err) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         console.log('Copy successful!');
+//       }
+//     });
+//   });
+// });
+
+
+const fs = require('fs');
+
+fs.readFile('greet.txt', 'utf-8', (err, data1) => {
+  fs.readFile('file.txt', 'utf-8', (err, data2) => {
     const finalContent = data1 + '\n' + data2;
 
     fs.writeFile('thirdfile.txt', finalContent, (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('Copy successful!');
-      }
+      console.log('Copy successful!');
     });
   });
 });
