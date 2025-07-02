77da93130466d720ca634d0218ad56b9766af842
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-07-02 09:21:27 +0000

    commit

---
+++ modules/fs/assignment1.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const f1 = fs.readFileSync('greet.txt', 'utf-8');
+
+const f2 = fs.readFileSync('file.txt', 'utf-8');
+
+const finalContent = f1 +'\n'+ f2;
+
+fs.writeFileSync('thirdfile.txt', finalContent);
+
+console.log('copying successful!');
