c4bb245b85fad0cbbf34b57c2e89d22957b4d5dc
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-07-04 09:46:50 +0000

    commit

---
+++ modules/fs/assignment5.js
@@ -0,0 +1,16 @@
+// runs continuously and logs the current timestamp to a file named activity.log every 2 seconds
+
+const fs = require('fs');
+const logFile = 'activity.log';
+function logTimestamp() {
+    const timestamp = new Date().toLocaleString();
+    fs.appendFile(logFile, `${timestamp} \n`, (err) => {
+        if (err) {
+            console.error('Error writing to log file:', err); 
+        }
+    });
+}
+
+setInterval(logTimestamp, 1000);
+
+
