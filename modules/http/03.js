a86aecfab569467d07132be86af091e9605ff517
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-07-10 06:10:53 +0000

    commit

--- modules/http/03.js
+++ modules/http/03.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.get("/", (req, res)=>{
+    // res.sendFile(__dirname, "/login.html")
+    // res.send({"Name": "Sanjay Gupta"});
+    // res.json({"Name":"Sneha"});
+    // res.status(200).send({"Sanjay":"Gupta"});
+})
+
 
 
 app.listen(3000, ()=>{
