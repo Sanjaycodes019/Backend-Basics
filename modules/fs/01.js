43d82d7e77d77b483cea9d0ea62d26ecf8fdbf48
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-07-14 08:28:43 +0000

    commit

---
+++ modules/fs/01.js
@@ -0,0 +1,26 @@
+// fs module provide api to interact with file system
+// reading, writing, creating, deleting files and directories
+// synchronous(blocking) - easy to read and understand
+// asynchronous(non-blocking) - better performance, real-world usage
+
+
+
+// importing the fs module
+ const fs = require('fs');
+ const fsPromises = require('fs').promises;
+
+ // writing to a file
+ // sync version
+ fs.writeFileSync('demo.txt', 'hello from node!');
+
+ // async version
+ fs.writeFile('demo1.txt', 'hello from nodejs(async', (err)=>{
+    if(!err) console.log('file written sucessully!');
+ });
+
+ // promise version
+ async function writeFile(){
+    await fsPromises.writeFile('demo3.txt', 'hello from fs promises');
+    console.log('done form promise!')
+ }
+ writeFile();
