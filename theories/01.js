817b859437340d91c39dfa64327d5206aa77ecad
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-08-07 09:43:00 +0000

    commit

--- theories/01.js
+++ theories/01.js
@@ -1,14 +1,24 @@
-const http = require('http')
+const express = require('express');
+const app = express();
 
-http.createServer((req, res)=>{
-    if(req.url === '/hello'){
-        res.writeHead(200, {'content-type':'text/plain'});
-        res.end('Hello User')
-    } else if(req.url === '/about'){
-        res.writeHead(200, {'content-type':'text/plain'});
-        res.end('About Page')
-    } else {
-        res.writeHead(404, {'content-type':'text/plain'});
-        res.end('404 - Route error');
-    }
-}).listen(3000)
+app.use(express.json());
+
+// Params
+app.get('/user/:id', (req, res) => {
+    res.send(`User ID is ${req.params.id}`);
+});
+
+// Query
+app.get('/search', (req, res) => {
+    res.send(`Search for ${req.query.q}`);
+});
+
+// Body
+app.post('/register', (req, res) => {
+    const { name, email } = req.body;
+    res.send(`User ${name} with email ${email} registered!`);
+});
+
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
