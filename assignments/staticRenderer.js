c00ffd68585bf526298dc10a1a125515c3a0c302
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-08-07 10:10:15 +0000

    commit

--- assignments/staticRenderer.js
+++ assignments/staticRenderer.js
@@ -1,14 +1,47 @@
+// const express = require('express')
+// const path = require('path')
+// const app = express()
+
+// app.use(express.static(path.join(__dirname, 'public')))
+
+// app.get('/', (req, res)=>{
+//     res.sendFile(path.join(__dirname, 'index.html'))
+// })
+
+// app.listen(3000, ()=>{
+//     console.log('server running on the port 3000')
+// })
+
+
+
+// if its said don't use express.static
+// we have to serve is manually
+
 const express = require('express')
 const path = require('path')
+const { runInNewContext } = require('vm')
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'public')))
-
+// serve html manually
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-app.listen(3000, ()=>{
-    console.log('server running on the port 3000')
+// serve css manually
+app.get('/style.css', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'style.css'))
 })
+
+// serve js manually
+app.get('/script.js', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'script.js'))
+})
+
+app.listen(3000, ()=>{
+    console.log('sever running on the port 3000')
+})
+
+// note
+// res.sendFile() works for any file type — HTML, CSS, JS, image, PDF, etc.
+// You just need to give the correct absolute path using path.join() and __dirname.
 
