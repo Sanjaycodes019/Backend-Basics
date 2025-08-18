9b6491b1aab99fadc9d38c6afcdcedf67d74ce95
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-08-18 05:09:44 +0000

    commit

--- pagination/server.js
+++ pagination/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const { start } = require("repl");
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -10,21 +11,38 @@ const projects = Array.from({ length: 100 }, (_, i) => ({
   description: `This is the description of project ${i + 1}`
 }));
 
-app.get("/get-products", (req, res) => {
-  let page = parseInt(req.query.page) || 1;  
-  let limit = parseInt(req.query.limit) || 10; 
+// app.get("/get-products", (req, res) => {
+//   let page = parseInt(req.query.page) || 1;  
+//   let limit = parseInt(req.query.limit) || 10; 
+
+//   let startIndex = (page - 1) * limit;
+//   let endIndex = page * limit;
+
+//   let result = projects.slice(startIndex, endIndex);
+
+//   res.json({
+//     page,
+//     limit,
+//     total: projects.length,
+//     data: result
+//   });
+// });
+
+app.get("/get-products", (req, res)=>{
+  let page = parseInt(req.query.page) || 1;
+  let limit = parseInt(req.query.limit) || 10;
 
   let startIndex = (page - 1) * limit;
-  let endIndex = page * limit;
+  let endIndex = page*limit;
 
   let result = projects.slice(startIndex, endIndex);
 
   res.json({
     page,
-    limit,
-    total: projects.length,
+    limit, 
+    total: projects.length, 
     data: result
-  });
-});
+  })
+})
 
 app.listen(3000, () => console.log("Server running on http://localhost:3000"));
