8396553db40ed3b453955afd58a50c496431c4d5
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-12-11 09:03:46 +0000

    commit

--- theories/01.js
+++ theories/01.js
@@ -18,26 +18,3 @@
 // });
 
 
-
-// serving static files using 
-
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = 3000
-
-app.get('/home', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'home.html'))
-})
-
-app.get('/about', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'about.html'))
-})
-
-app.get('/contact', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'contact.html'))
-})
-
-app.listen(port, ()=>{
-    console.log('server running on the port 3000')
-})
